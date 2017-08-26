0a45875eb9f9605336abae3feec8fb374c3b1efc
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-08-26 20:08:13 +0100

    Add more optimized and less api-dependent implementation for insertion-sort

--- algorithms/insertion_sort.rb
+++ algorithms/insertion_sort.rb
@@ -1,6 +1,35 @@
 class InsertionSort
   class << self
     def sort(array)
+      sorted = [array[0]]
+      array = array[1..-1]
+
+      i = 0
+      while s = sorted[i]
+        c = array[0]
+
+        break unless c
+
+        if s < c
+          sorted[i + 1] = c
+          array = array[1..-1]
+        else
+          j = 0
+          while sorted[j]
+            next(j += 1) if sorted[j] < c
+            temp = sorted[j..-1]
+            sorted[j] = c
+            sorted[j+1..-1] = temp
+            break
+          end
+
+          array = array[1..-1]
+        end
+
+        i += 1
+      end
+
+      sorted
     end
   end
 end
