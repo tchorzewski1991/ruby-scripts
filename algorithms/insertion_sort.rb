57ddda4d25003f6477e1bf8ff3a5ed9db5f880b8
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-08-26 19:08:43 +0100

    Remove current implementation of insertion sort

--- algorithms/insertion_sort.rb
+++ algorithms/insertion_sort.rb
@@ -1,22 +1,6 @@
 class InsertionSort
   class << self
     def sort(array)
-      sorted = [array[0]]
-      i = 1
-
-      while item = array[i]
-        sorted.length.times do |i|
-          next if sorted[i] <= item
-          temp = sorted[i..-1]
-          sorted[i] = item
-          sorted[i+1..-1] = temp
-          break
-        end
-
-        i += 1
-      end
-
-      sorted
     end
   end
 end
