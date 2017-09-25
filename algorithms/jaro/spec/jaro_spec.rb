c7dcec5436c7da7f29b408fae1ddb4a6ba74546a
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-09-25 21:43:01 +0200

    Simple set of specs for Jaro distance measurment
    -> those data examples refers to reliable sources

---
+++ algorithms/jaro/spec/jaro_spec.rb
@@ -0,0 +1,42 @@
+require './jaro'
+
+RSpec.describe Jaro do
+  subject { Jaro }
+
+  context 'when transposition encountered' do
+    let(:with_transposition) {
+      {
+        :similarity => 0.733,
+        :source => 'CRATE',
+        :target => 'TRACE'
+      }
+    }
+
+    it 'expects to return correct similarity value' do
+      _source = with_transposition[:source]
+      _target = with_transposition[:target]
+      _similarity = with_transposition[:similarity]
+
+      expect(subject.new(_source, _target).distance).to eq(_similarity)
+    end
+  end
+
+  context 'when transposition not occures' do
+    # Data reference: http://rosettacode.org/wiki/Jaro_distance
+    let(:without_transposition) {
+      {
+        :similarity => 0.822,
+        :source => 'DWAYNE',
+        :target => 'DUANE'
+      }
+    }
+
+    it 'expects to return correct similarity value' do
+      _source = without_transposition[:source]
+      _target = without_transposition[:target]
+      _similarity = without_transposition[:similarity]
+
+      expect(subject.new(_source, _target).distance).to eq(_similarity)
+    end
+  end
+end
