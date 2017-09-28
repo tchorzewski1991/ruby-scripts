ee304515e0c9d0d504bea8ca29fd809a35d32a9c
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-09-28 19:38:43 +0100

    Add new, more optimized implementation for bubble sort algorithm

--- algorithms/bubble_sort.rb
+++ algorithms/bubble_sort.rb
@@ -1,6 +1,23 @@
 module BubbleSort
   class << self
     def sort(array)
+      j = array.length - 1
+      k = j - 1
+
+      k.times do |k|
+        i = 0
+        while i < j - k
+          m, n = array[i], array[i + 1]
+
+          next(i += 1) if m < n
+
+          array[i], array[i + 1] = n, m
+
+          i += 1
+        end
+      end
+
+      array
     end
   end
 end
