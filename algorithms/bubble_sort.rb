66d046701f81e0d373d7b1fffc4060bc7a56a973
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-09-28 19:48:13 +0100

    Comment section to clarify what's going on within current implementation

--- algorithms/bubble_sort.rb
+++ algorithms/bubble_sort.rb
@@ -1,7 +1,20 @@
 module BubbleSort
   class << self
     def sort(array)
+      # Variable j refers to number of comparsions which we need to
+      # consider during our sorting process. It is always reduced by one
+      # in relation to current array length
       j = array.length - 1
+
+      # Bubbling - how do we know, how many iterations we need to
+      # consider to get fully sorted array? Each single iteration moves
+      # the biggest element to the end of our target array. According to
+      # that simple fact it is enough to execute our 'while' loop at least
+      # j - k times. Variable k at start points out to ommiting last
+      # element. This is number of iterations without taking into account
+      # last element. For each 'while' loop variable k will reduce number of
+      # comparsion left, as we do not need to compare elements which
+      # are currently in the correct order.
       k = j - 1
 
       k.times do |k|
