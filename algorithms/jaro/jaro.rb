cb6b48559b9c75802525e36b09cf50422540d430
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-09-29 19:19:43 +0100

    Setup important variables description

--- algorithms/jaro/jaro.rb
+++ algorithms/jaro/jaro.rb
@@ -5,8 +5,17 @@ class Jaro
   end
 
   def distance
+    # Two characters from both strings respectively, are considered
+    # matching only if they are the same and not farther than
+    # (max(|s|, |t|) / 2)).floor - 1. By considering 'not farther than'
+    # we mean difference in index positions between two characters.
+    # Variable window refers to 'range' described above.
+    # Variable match_in_window is self describing.
     window = @window
 
+    # It's important to know that we always need to start iteration
+    # from shorter to longer text. Those two are encountered during
+    # initialization.
     shorter, longer = @source, @target
     shorter_length, longer_length = shorter.length, longer.length
 
