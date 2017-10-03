09dae1a2fd4412624778787b8d1dfc149e9f7479
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-10-03 19:38:23 +0100

    Implement part responsible for examine dot product and magnitudes

--- algorithms/word_cosine_similarity/word_cosine_similarity.rb
+++ algorithms/word_cosine_similarity/word_cosine_similarity.rb
@@ -22,6 +22,39 @@ class WordCosineSimilarity
         t_occurences[y] += 1
         j += 1
       end
+
+      numerator = 0
+
+      k = 0
+      while true
+        o = ords[k] or break
+        numerator += (s_occurences[o] * t_occurences[o])
+        k += 1
+      end
+
+      s_values = s_occurences.values
+      t_values = t_occurences.values
+
+      s_magnitude = 0
+      t_magnitude = 0
+
+      l = 0
+      while true
+        x = s_values[l]
+        y = t_values[l]
+
+        x || y || break
+
+        x && (s_magnitude += x**2)
+        y && (t_magnitude += y**2)
+
+        l += 1
+      end
+
+      s_magnitude = Math.sqrt(s_magnitude).to_f
+      t_magnitude = Math.sqrt(t_magnitude).to_f
+
+      (numerator / (s_magnitude * t_magnitude)).round(5)
     end
   end
 end
