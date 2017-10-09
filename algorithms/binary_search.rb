5de64b2a3dd7d33d9f8b51142851f0a4816da6ac
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-10-09 19:48:53 +0100

    BinarySearch performance improvements
    -> Unexpectedly using singleton method is much more
       performant than initializing new instance of the class
    
    -> Remove some unnecessary assignments
    
    -> Add benchmarks for comparsion with Array#bsearch method

--- algorithms/binary_search.rb
+++ algorithms/binary_search.rb
@@ -1,20 +1,39 @@
+require 'benchmark/ips'
+
 class BinarySearch
-  attr_reader :collection
+  class << self
+    def find(collection, element = nil)
+      c = collection
+      i = collection.length
 
-  def initialize(collection)
-    @collection = collection
-  end
+      while i > 0
+        i /= 2
 
-  def find(element)
-    c = collection
+        return element if element == c[i]
 
-    while true
-      i = c.length / 2
-
-      return element if element == c[i]
-      return if i == 0
-
-      c = c[i] < element ? c[i..-1] : c[0...i]
+        c = c[i] < element ? c[i..-1] : c[0...i]
+      end
     end
   end
 end
+
+Benchmark.ips do |x|
+  x.report('BinarySearch#find') do
+    BinarySearch.find([1,2,3,4,5,6,7,8], 7)
+  end
+
+  x.report('Array#bsearch') do
+    [1,2,3,4,5,6,7,8].bsearch { |x| x == 7 }
+  end
+
+  x.compare!
+end
+
+#
+# Calculating -------------------------------------
+#    BinarySearch#find      1.165M (± 8.2%) i/s -      5.836M in   5.046825s
+#        Array#bsearch      1.622M (± 5.7%) i/s -      8.154M in   5.042785s
+#
+# Comparison:
+#        Array#bsearch:  1622294.6 i/s
+#    BinarySearch#find:  1164808.4 i/s - 1.39x  slower
