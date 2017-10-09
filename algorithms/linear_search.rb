9b5a2a3a380ed20017150036e6d6376de3bd62d5
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-10-09 19:38:13 +0100

    LinearSearch performance improvements
    -> Unexpectedly using singleton method is much more
       performant than initializing new instance of the class
    
    -> Remove some unnecessary assignment within while conditional
       statement
    
    -> Remove some typos

--- algorithms/linear_search.rb
+++ algorithms/linear_search.rb
@@ -1,43 +1,48 @@
 require 'benchmark/ips'
 
 class LinearSearch
-  attr_reader :collection
+  class << self
 
-  def initialize(collection)
-    @collection = collection
-  end
+    def find(collection, element = nil)
+      i = 0
+      l = collection.length
 
-  def find(element = nil)
-    i = 0
+      while i < l
+        item = collection[i]
+        result = block_given? ? yield(item) : item == element
+        result && (return item)
 
-    while item = collection[i]
-      result = block_given? ? yield(item) : item == element
-      result && (return item)
-
-      i += 1
+        i += 1
+      end
     end
   end
 end
 
 Benchmark.ips do |x|
-  x.report('LinearSearch#find') do
-    LinearSearch.new([2,2,3,6,7,5,3]).find { |x| x == 7 }
+  x.report('LinearSearch#find with block') do
+    LinearSearch.find([2,2,3,6,7,5,3]) { |x| x == 7 }
   end
 
-  x.report('Array#find') do
+  x.report('LinearSearch#find without block') do
+    LinearSearch.find([2,2,3,6,7,5,3], 7)
+  end
+
+  # Unexpectedly ruby Enumerable#find can't return result of the search
+  # when invoked without a block
+
+  x.report('Enumerable#find') do
     [2,2,3,6,7,5,3].find { |x| x == 7 }
   end
 
   x.compare!
 end
 
-# Warming up --------------------------------------
-#              my find    70.376k i/100ms
-#            ruby find    69.078k i/100ms
 # Calculating -------------------------------------
-#              my find    951.078k (± 7.0%) i/s -      4.786M in   5.060166s
-#            ruby find    941.165k (± 6.8%) i/s -      4.697M in   5.015971s
+# LinearSearch#find with block      970.754k (± 6.5%) i/s -      4.877M in   5.046184s
+# LinearSearch#find without block     1.158M (± 5.9%) i/s -      5.835M in   5.057635s
+# Enumerable#find                   921.794k (± 7.0%) i/s -      4.627M in   5.044513s
 #
 # Comparison:
-#              my find:   951078.2 i/s
-#            ruby find:   941164.9 i/s - same-ish: difference falls within error
+# LinearSearch#find without block:  1157772.7 i/s
+# LinearSearch#find with block:     970753.9 i/s - 1.19x  slower
+# Enumerable#find:                  921793.9 i/s - 1.26x  slower
