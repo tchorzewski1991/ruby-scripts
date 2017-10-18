dc58a570080a45938d2cc97b31eb1727c1b56721
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-10-18 19:13:13 +0100

    Add benchmarks for string-operations: removing first char

--- performance/string_operations.rb
+++ performance/string_operations.rb
@@ -61,3 +61,27 @@ Benchmark.ips do |x|
 
   x.compare!
 end
+#
+# Calculating -------------------------------------
+#            String#[]      6.602M (± 9.1%) i/s -     32.855M in   5.027235s
+#         String#slice      6.405M (± 8.3%) i/s -     31.871M in   5.017099s
+#        Srting#slice!      5.996M (± 8.6%) i/s -     29.800M in   5.014456s
+#          String#gsub      1.527M (± 8.3%) i/s -      7.603M in   5.019654s
+#         String#gsub!      1.607M (± 6.2%) i/s -      8.087M in   5.051857s
+#           String#sub      1.472M (± 8.4%) i/s -      7.371M in   5.047246s
+#          String#sub!      1.544M (± 8.4%) i/s -      7.681M in   5.014661s
+#           String#[]=      2.513M (± 8.2%) i/s -     12.551M in   5.031050s
+#         String#split      1.709M (± 9.8%) i/s -      8.525M in   5.039068s
+#          String#ease      5.464M (± 8.5%) i/s -     27.138M in   5.006263s
+#
+# Comparison:
+#            String#[]:  6601985.6 i/s
+#         String#slice:  6404593.1 i/s - same-ish: difference falls within error
+#        Srting#slice!:  5996043.4 i/s - same-ish: difference falls within error
+#          String#ease:  5464458.1 i/s - 1.21x  slower
+#           String#[]=:  2512528.2 i/s - 2.63x  slower
+#         String#split:  1708825.6 i/s - 3.86x  slower
+#         String#gsub!:  1607419.0 i/s - 4.11x  slower
+#          String#sub!:  1543953.8 i/s - 4.28x  slower
+#          String#gsub:  1526774.2 i/s - 4.32x  slower
+#           String#sub:  1472456.8 i/s - 4.48x  slower
