fc6ea09e66ded44598b68f14d88b6ca2b102e9cb
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-14 21:53:22 +0100

    Small update for Interpose#compute description

---
+++ enumerations/interpose/interpose.rb
@@ -0,0 +1,53 @@
+module Interpose
+  extend self
+
+  # Interpose#compute creates enumeration that allows for separator insertion
+  # between each neighbour elements of given collection. It uses nil as default
+  # separator.
+
+  # Example: Basic usage
+  #   Returns new array with collection elements and nil as separator
+  #
+  #   Interpose.compute [1, 2, 3]
+  #   => [1, nil, 2, nil, 3]
+  #
+  # Example: With 'separator' given from optional hash
+  #   Returns new array with collection elements and 'separator' as separator
+  #
+  #   Interpose.compute [1, 2, 3], separator: 'separator'
+  #   => [1, 'separator', 2, 'separator', 3]
+  #
+  # Example: For empty collection
+  #   Returns new array without any elements
+  #
+  #   Interpose.compute []
+  #   => []
+  #
+  # Example: For collection with only one element
+  #   Returns new array with only one element
+  #
+  #   Interpose.compute [1]
+  #   => [1]
+
+  def compute(collection = [], **opts)
+    separator = opts.fetch(:separator, nil)
+
+    enum = Enumerator.new do |yielder|
+      length = collection.length
+      k = 0
+
+      while k < length
+        element = collection[k]
+
+        next(
+          yielder << element
+          k += 1
+        ) if length == 1
+
+        yielder << element
+        yielder << separator if k.succ < length
+        k += 1
+      end
+    end
+  end
+end
