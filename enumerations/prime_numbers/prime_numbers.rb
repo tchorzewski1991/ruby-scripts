e6ed54d7a960ab7ad2e2ba2dc273eaa209861e91
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-14 22:18:43 +0100

    Add description for PrimeNumbers#compute method

--- enumerations/prime_numbers/prime_numbers.rb
+++ enumerations/prime_numbers/prime_numbers.rb
@@ -1,6 +1,28 @@
 module PrimeNumbers
   extend self
 
+  # PrimeNumbers#compute creates enumerator that allows you for computation of
+  # first n-th prime numbers. Returning enumerator instead of enumeration is
+  # just an implementation detail and could be easily expanded with implicit
+  # block argument.
+
+  # Example: Basic usage
+  #   Returns enum for n first prime numbers
+  #
+  #   PrimeNumbers.compute(3)
+  #   => #<Enumerator: (...):each>
+  #
+  #   PrimeNumbers.compute(3).first(2)
+  #   => [2, 3]
+  #
+  # Example: With block of code for additional transformation
+  #   Returns result of enumeration for first n prime numbers
+  #
+  #   PrimeNumbers.compute(3) do |enum|
+  #     enum.select { |prime| prime > 2 }
+  #   end
+  #   => [3, 5]
+
   def compute(n)
     enum = Enumerator.new do |yielder|
       yielder << 2
