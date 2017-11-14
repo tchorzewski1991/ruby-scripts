fd0c27fe3f161047ad30a789f38c421c0719cbf1
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-14 22:19:43 +0100

    Add benchmarks for comparsion with existing Prime std library

--- enumerations/prime_numbers/prime_numbers.rb
+++ enumerations/prime_numbers/prime_numbers.rb
@@ -1,3 +1,6 @@
+require 'benchmark/ips'
+require 'prime'
+
 module PrimeNumbers
   extend self
 
@@ -61,3 +64,58 @@ module PrimeNumbers
     block_given? && yield(enum) || enum
   end
 end
+
+# I suppose that those benchmarks are quite satisfying if taken into account
+# thats only 50 lines of code. Original Prime library has lots of additional
+# improvements just like caching for example, or hardcoded small n-th primes.
+# This possibly could be one of the reason of slower computations.
+# Please welcome to contribute if you recognize some bugs or significant
+# improvements :)
+
+# Benchmark.ips do |x|
+#   n = 10
+#
+#   x.report 'Prime' do
+#     Prime::TrialDivisionGenerator.new.first(n)
+#   end
+#
+#   x.report 'PrimeNumbers' do
+#     PrimeNumbers.compute(n).first(n)
+#   end
+#
+#   x.compare!
+# end
+#
+# Benchmark.ips do |x|
+#   n = 100
+#
+#   x.report 'Prime' do
+#     Prime::TrialDivisionGenerator.new.first(n)
+#   end
+#
+#   x.report 'PrimeNumbers' do
+#     PrimeNumbers.compute(n).first(n)
+#   end
+#
+#   x.compare!
+# end
+
+#
+# Calculating -------------------------------------
+#                Prime    261.634k (± 4.0%) i/s -      1.330M in   5.093276s
+#         PrimeNumbers     80.773k (± 3.7%) i/s -    407.316k in   5.050198s
+#
+# Comparison:
+#                Prime:   261633.6 i/s
+#         PrimeNumbers:    80773.1 i/s - 3.24x  slower
+#
+# Warming up --------------------------------------
+#                Prime     3.656k i/100ms
+#         PrimeNumbers   499.000  i/100ms
+# Calculating -------------------------------------
+#                Prime     35.798k (± 7.8%) i/s -    179.144k in   5.038630s
+#         PrimeNumbers      5.018k (± 5.3%) i/s -     25.449k in   5.086792s
+#
+# Comparison:
+#                Prime:    35798.2 i/s
+#         PrimeNumbers:     5017.9 i/s - 7.13x  slower
