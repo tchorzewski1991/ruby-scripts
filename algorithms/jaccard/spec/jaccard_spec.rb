0c443927479841a8002dfd67192cd5357736d587
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-16 19:49:13 +0100

    Meager lexical changes for Jaccard module specs

--- algorithms/jaccard/spec/jaccard_spec.rb
+++ algorithms/jaccard/spec/jaccard_spec.rb
@@ -2,13 +2,13 @@ require './jaccard'
 
 RSpec.describe Jaccard do
   describe 'jaccard similarity coefficient' do
-    it 'expects to return correct similarity for apple-applet' do
+    it 'returns correct value for apple-applet' do
       expect(Jaccard::Words.coefficient('apple', 'applet')).to eq(0.8)
     end
   end
 
   describe 'jaccard distance (dissimilarity)' do
-    it 'expects to return correct distance for apple-applet' do
+    it 'returns correct value for apple-applet' do
       expect(Jaccard::Words.distance('apple', 'applet')).to eq(0.2)
     end
   end
