47d59b00a867803d979668bfe431d60549935c2b
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-16 19:38:43 +0100

    Add basic description and usage for Hamming module

--- algorithms/hamming/hamming.rb
+++ algorithms/hamming/hamming.rb
@@ -1,6 +1,17 @@
 module Hamming
   extend self
 
+  # Hamming#distance measures the minimum number of substitutions required to
+  # change one string into another, or the minimum number of errors that could
+  # have transformed one string into the other.
+
+  # Example: Basic usage
+  #   Returns number of different characters from both strings of equal length
+  #
+  #   Hamming.distance 'karolin', 'kathrin'
+  #   => 3
+  #
+
   def distance(origin, target)
     j, k = origin.length, target.length
 
