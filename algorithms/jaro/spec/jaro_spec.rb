25b1cac01912815489a7d06be9b882d28322da7d
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-17 19:59:13 +0100

    More clarification and cohesion for Jaro module specs

--- algorithms/jaro/spec/jaro_spec.rb
+++ algorithms/jaro/spec/jaro_spec.rb
@@ -3,56 +3,43 @@ require './jaro'
 RSpec.describe Jaro do
   subject { Jaro }
 
-  context 'when transposition encountered' do
-    let(:with_transposition) {
-      {
-        :similarity => 0.94444,
-        :source => 'MARTHA',
-        :target => 'MARHTA'
-      }
+  let(:with_transposition) do
+    { :similarity => 0.94444, :origin => 'MARTHA', :target => 'MARHTA' }
+  end
+
+  # Data reference: http://rosettacode.org/wiki/Jaro_distance
+  let(:without_transposition) do
+    {
+      1 => { :similarity => 0.82222, :origin => 'DWAYNE', :target => 'DUANE' },
+      2 => { :similarity => 0.73333, :origin => 'CRATE',  :target => 'TRACE' }
     }
+  end
 
+  context 'when transposition encountered' do
     it 'returns correct similarity value' do
-      _source = with_transposition[:source]
-      _target = with_transposition[:target]
-      _similarity = with_transposition[:similarity]
+      similarity = with_transposition[:similarity]
+      origin     = with_transposition[:origin]
+      target     = with_transposition[:target]
 
-      expect(subject.new(_source, _target).distance).to eq(_similarity)
+      expect(subject.new(origin, target).distance).to eq(similarity)
     end
   end
 
   context 'when transposition not occures' do
-    # Data reference: http://rosettacode.org/wiki/Jaro_distance
-    let(:without_transposition) {
-      {
-        :similarity => 0.82222,
-        :source => 'DWAYNE',
-        :target => 'DUANE'
-      }
-    }
-
     it 'returns correct similarity value' do
-      _source = without_transposition[:source]
-      _target = without_transposition[:target]
-      _similarity = without_transposition[:similarity]
+      similarity = without_transposition[1][:similarity]
+      origin     = without_transposition[1][:origin]
+      target     = without_transposition[1][:target]
 
-      expect(subject.new(_source, _target).distance).to eq(_similarity)
+      expect(subject.new(origin, target).distance).to eq(similarity)
     end
 
-    let(:without_transposition) {
-      {
-        :similarity => 0.73333,
-        :source => 'CRATE',
-        :target => 'TRACE'
-      }
-    }
-
     it 'returns correct similarity value' do
-      _source = without_transposition[:source]
-      _target = without_transposition[:target]
-      _similarity = without_transposition[:similarity]
+      similarity = without_transposition[2][:similarity]
+      origin     = without_transposition[2][:origin]
+      target     = without_transposition[2][:target]
 
-      expect(subject.new(_source, _target).distance).to eq(_similarity)
+      expect(subject.new(origin, target).distance).to eq(similarity)
     end
   end
 end
