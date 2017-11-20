89ae654b5f37e9cf2039d5fa59b3b52512ebe66a
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-20 22:10:43 +0100

    Add benchmarks for occurence counting

--- performance/occurence_counting.rb
+++ performance/occurence_counting.rb
@@ -23,3 +23,33 @@ end
 def variant_6(array)
   Hash[array.group_by(&:itself).map { |key, value| [key, value.size] }]
 end
+
+Benchmark.ips do |x|
+  array = %w(a a b c d e r e s a s e)
+
+  x.report('variant 1') { variant_1(array) }
+  x.report('variant 2') { variant_2(array) }
+  x.report('variant 3') { variant_3(array) }
+  x.report('variant 4') { variant_4(array) }
+  x.report('variant 5') { variant_5(array) }
+  x.report('variant 6') { variant_6(array) }
+
+  x.compare!
+end
+
+#
+# Calculating -------------------------------------
+#                    1     86.668k (± 6.3%) i/s -    438.932k in   5.085439s
+#                    2    241.159k (± 5.6%) i/s -      1.211M in   5.037488s
+#                    3    215.225k (± 8.2%) i/s -      1.072M in   5.020525s
+#                    4    216.927k (± 6.7%) i/s -      1.090M in   5.049314s
+#                    5    178.323k (± 5.2%) i/s -    891.884k in   5.015653s
+#                    6    132.919k (± 4.7%) i/s -    663.572k in   5.003539s
+#
+# Comparison:
+#                    2:   241159.1 i/s
+#                    4:   216927.4 i/s - same-ish: difference falls within error
+#                    3:   215225.2 i/s - same-ish: difference falls within error
+#                    5:   178322.5 i/s - 1.35x  slower
+#                    6:   132919.2 i/s - 1.81x  slower
+#                    1:    86668.1 i/s - 2.78x  slower
