8000cdcde22c34c51dc04eedb31d66637f5b10cd
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-22 22:18:13 +0100

    Implelent experimantal value object for input data

--- algorithms/luhn/luhn.rb
+++ algorithms/luhn/luhn.rb
@@ -1,18 +1,43 @@
 module Luhn
   extend self
 
-  def valid?(input)
-    sum, i = 0, 0
-
-    while c = input[i]
-      c = c.to_i
-      sum += begin
-        i % 2 == 0 &&
-          (c = c * 2 and c > 9 ? c -= 9 : c) || c
-      end
-      i += 1
+  class Input
+    def initialize(input)
+      @input = String(input)
+        .delete(' ')
+        .delete('_')
     end
 
-    (sum % 10).zero?
+    def [](index)
+      input[index]
+    end
+
+    def valid?
+      (input !~ /\D/) & (input =~ /\d+/)
+    end
+
+    private
+    attr_reader :input
+  end
+
+  def valid?(input)
+    input = Input.new(input)
+
+    if input.valid?
+      sum, i = 0, 0
+
+      while c = input[i]
+        c = c.to_i
+        sum += begin
+          i % 2 == 0 &&
+            (c = c * 2 and c > 9 ? c -= 9 : c) || c
+        end
+        i += 1
+      end
+
+      (sum % 10).zero?
+    else
+      false
+    end
   end
 end
