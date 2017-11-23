c6dc20337c7aa358480facef23ec6a9de852267b
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-23 22:18:14 +0100

    Improve actual description and notes on algorithm internals

--- algorithms/knuth-morris-pratt/knuth_morris_pratt.rb
+++ algorithms/knuth-morris-pratt/knuth_morris_pratt.rb
@@ -4,26 +4,26 @@ module KnuthMorrisPratt
   def compute(origin, target)
     k, j = origin.length, target.length
 
-    # Variable cs (common-subsequence) will refer to the longest
-    # common subsequence that has been found
+    # Variable common_subsequence refers to the longest common
+    # subsequence which we would like to find in our origin string.
     common_subsequence = nil
 
-    # Variable m refers to current index position in our source string
-    # that will be searched. It is position where prospective match
-    # for our target begins
+    # Variable m refers to index position in our source string.
+    # It is the position where prospective match for our target string
+    # begins.
     m = 0
 
     while m < k
-      # For performance boost we can skip iteration for position
-      # within the source which does not match potential begin of
-      # our target string
+      # For performance boost we can skip iterating through positions
+      # where the origin string does not match potential begin of
+      # our target string.
       next(m += 1) if origin[m] != target[0]
 
-      # Variable temp_cs refers to temporary common subsequence
-      # matched within each provided iteration
+      # Variable temporary_subsequence refers to string (common
+      # subsequence) matched within current iteration
       temporary_subsequence = ''
 
-      # Variable i refers to current position in our target string
+      # Variable i refers to position in our target string
       # whom common subsequence we would like to find
       i = 0
 
@@ -31,10 +31,11 @@ module KnuthMorrisPratt
         target_char = target[i]
 
         # Expanding/Looking for common subsequence occure until the same
-        # position within source and target are different. When different
-        # characters will be encountered, 'else' branch of  the conditional
-        # expression is executed, and position relative to the source string
-        # is updated with value equals to length of current 'matches'.
+        # position within origin and target strings are different. When
+        # different characters will be encountered, 'else' branch of the
+        # conditional statement is executed, and position relative to the
+        # origin string is updated with value equals to length of current
+        # 'matches'.
         if origin[m + i] == target_char
           temporary_subsequence << target_char
           i += 1
