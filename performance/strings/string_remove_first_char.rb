c7e3de427d3b99801babd537f57d1cb8afc23d93
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-12-14 18:26:11 +0100

    Remove some incorrect implementations
    -> I was assuming that we want to remove first character from
       the string in any way. There was an inaccuracy between
       benchmarked examples and provided description.

--- performance/strings/string_remove_first_char.rb
+++ performance/strings/string_remove_first_char.rb
@@ -27,28 +27,12 @@ Benchmark.ips do |x|
     s = str.slice(1, str.length - 1)
   end
 
-  x.report 'Srting#slice!' do
-    str.slice!(1, str.length - 1)
-  end
-
   x.report 'String#gsub' do
     s = str.gsub(/^./, '')
   end
 
-  x.report 'String#gsub!' do
-    str.gsub!(/^./, '')
-  end
-
   x.report 'String#sub' do
     s = str.sub(/^./, '')
-  end
-
-  x.report 'String#sub!' do
-    str.sub!(/^./, '')
-  end
-
-  x.report 'String#[]=' do
-    str.tap { |x| x[0] = '' }
   end
 
   x.report 'String#split' do
