0fe711c25dcd6535e186083bf2c3c81256126baa
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-08 11:06:19 +0100

    Slight improve description and usage examples.

--- algorithms/text-processing/jaccard/jaccard.rb
+++ algorithms/text-processing/jaccard/jaccard.rb
@@ -2,24 +2,33 @@ module Jaccard
   module Words
     DEFAULT_ROUND = 5
 
-    # The Jaccard coefficient measures similarity between finite sample sets,
-    # and is defined as the size of the intersection divided by the size of
-    # the union of the sample sets. Algorithm itself is also known as the
-    # Jaccard index or the Jaccard similarity coefficient. Algorithm refers
-    # to a statistic used for comparing the similarity and diversity of
-    # sample collections.
+    # The Jaccard coefficient measures similarity between finite sample
+    # sets. It is defined as the size of the intersection divided by the
+    # size of the union of the sample sets. Algorithm itself is also known
+    # as the Jaccard index or the Jaccard similarity coefficient. Algorithm
+    # refers to a statistic used for comparing the similarity and diversity
+    # of sample collections.
     #
-    # Example: Basic usage
-    #   Returns value of similarity between origin and target
+    # Example:
+    #   Returns value of similarity between origin and target with
+    #   default rounding by 5.
     #
     #   Jaccard::Words.coefficient 'apple', 'applet'
-    #   => 0.8
+    #   => 0.80000 (unfortunately those 0 will be cutted out.)
     #
-    # Example: Basic usage
-    #   Returns value of distance between origin and target
+    # Example:
+    #   Returns value of distance between origin and target with
+    #   custom rounding by 2.
     #
-    #   Jaccard::Words.distance 'apple', 'applet'
-    #   => 0.2
+    #   Jaccard::Words.distance 'ala', 'ola', round: 2
+    #   => 0.33
+    #
+    # Example:
+    #   Returns value of similarity between origin and target with
+    #   disabled rounding.
+    #
+    #   Jaccard::Words.coefficient 'ala', 'ola', round: false
+    #   => 0.6666666666666666
 
     def self.coefficient(origin, target, round: DEFAULT_ROUND)
       # Variables origin_chars, and target_chars refer to temporary containers
@@ -46,6 +55,10 @@ module Jaccard
     end
 
     private
+
+    # _round_by_value method refers to my personal prototyping / searching /
+    # learning on various DRYing up techniques. I skipped a little bit of
+    # repetitive code by implementing clousure.
 
     def self._round_by_value(value)
       return yield unless value
