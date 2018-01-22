0d82b95aa28c8cd2e454bfcc5fa0c2328f65952d
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-22 16:31:21 +0100

    Describe pros/cons for eagerly loaded implementation

--- patterns/singleton/implementations/eager_instantiation.rb
+++ patterns/singleton/implementations/eager_instantiation.rb
@@ -1,4 +1,18 @@
 module Singleton
+  # This example refers to 'eagerly instantiateable' implementation
+  # of singleton object. Very similar example could be found in GOF
+  # interpretation of singleton pattern.
+  #
+  # Adventages
+  # - It is quite understandable, what's exactly going on.
+  # - It presents the purpose and nature of the object easily.
+  #
+  # Disadvantages
+  # - Eagerly instantiated. It will be loaded even, if we never use it.
+  # - Not testable. In case of unit test we should always provide
+  #   fresh environment and untouched state of objects. It is not
+  #   possible to accomplish that with current implementation.
+
   class EagerInstantiation
     @@_instance = EagerInstantiation.new
 
