c088133180ac66354dc238f94515566637a78fed
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-22 16:08:13 +0100

    Add quick overview of singleton pattern

---
+++ patterns/singleton/singleton.rb
@@ -0,0 +1,18 @@
+# Singleton pattern refers to programming technique, which
+# main point is to create standalone, unique instance of
+# the Ruby class. It becomes globally accessible due to
+# public accessor and contains predefined, expected behavior.
+
+# Singleton pattern could be really helpful for our system
+# if we exactly know, how to use it and what are the common
+# pitfalls due to overusage of singletons.
+
+# There are a lot of ways to define singleton.
+# In most cases it will be defined as an object of the Class
+# instance, but there is no any restriction to define it as
+# an instance of the Module. (Modules in Ruby are objects too).
+# It's worth to point out that both cases have some pros and cons,
+# so there is no perfect way, to define singleton.
+
+module Singleton
+end
