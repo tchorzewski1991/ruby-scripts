9c3ac5900690edabda80b13e6b550f470b66e5a9
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-22 17:01:31 +0100

    Describe pros/cons of lazily loaded implementation

--- patterns/singleton/implementations/lazy_instantiation.rb
+++ patterns/singleton/implementations/lazy_instantiation.rb
@@ -1,4 +1,20 @@
 module Singleton
+  # This example refers to 'lazily instantiateable' implementation
+  # of singleton object. Pretty congruous example could be found in
+  # GOF interpretation of singleton pattern.
+  #
+  # Adventages
+  # - It presents the purpose and nature of the object easily.
+  # - It is instantiated lazily, which means instance of the
+  #   LazyInstantiation class won't be created until the first
+  #   call to LazyInstantiation#instance method happens.
+  #
+  # Disadvantages
+  # - Need for being familiar with '||=' operator.
+  # - not testable. In case of unit test we should always provide
+  #   fresh environment and untouched state of objects. It is not
+  #   possible to accomplish that with current implementation.
+
   class LazyInstantiation
     def  self.instance
       @_instance ||= LazyInstantiation.new
