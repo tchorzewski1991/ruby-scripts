b62a70863fa7c3e1269ef8dc3b729fc9978d4e4a
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-23 11:34:28 +0100

    Describe pros/cons of modular §implementation

--- patterns/singleton/implementations/modular.rb
+++ patterns/singleton/implementations/modular.rb
@@ -1,4 +1,23 @@
 module Singleton
+  # This example refers to 'modular' implementation of
+  # singleton object. This is my prototyping on this
+  # subject.
+  #
+  # Adventages
+  # - No need to worry about #new method, as this is method of
+  #   the Class instance, not the Module instance.
+  # - Testable. Singleton::Modular#test method gives us access
+  #   and ability to create new, fresh instances of the _klass
+  #   class which has already predefined behavior.
+  # - _klass class instance is instantiated lazily, which means
+  #    it won't be created until the first call to Modular#instance
+  #    method happens.
+  #
+  # Disadvantages
+  # - Strongly depends on metaprogramming skills.
+  # - Can be included, extend, or prepended as this is
+  #   a mixin.
+
   module Modular
     class << self
       # Returns lazily created instance of _klass class.
