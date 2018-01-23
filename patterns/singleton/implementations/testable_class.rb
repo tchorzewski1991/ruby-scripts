e16ffe7807edef84b813ece19e87cddbda572a78
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-23 10:21:19 +0100

    Describe pros/cons of testable implementation

--- patterns/singleton/implementations/testable_class.rb
+++ patterns/singleton/implementations/testable_class.rb
@@ -1,4 +1,18 @@
 module Singleton
+  # This example refers to 'testable' implementation of singleton
+  # object.
+  #
+  # Adventages
+  # - It keeps 'behavior' in separate class, which makes it
+  #   easily testable in standard way.
+  # - It is instantiated lazily, which means instance of the
+  #   SingletonTestableClass class won't be created until the first
+  #   call to SingletonTestableClass#instance method happens.
+  #
+  # Disadvantages
+  # - It is not unique because of instantiateable TestableClass,
+  #   so it doesn't show unique nature of the object anymore?
+
   class TestableClass
   end
 
